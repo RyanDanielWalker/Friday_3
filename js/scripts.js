6c729bd13b5428d2d03cff6cefd5664e970c1b17
Ryan Walker <ryandanielwalker@gmail.com>
2021-04-02 11:03:33 -0700

    WIP: testing code.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,25 @@
 function mrRobogersSays(userInput) {
-  let inputArray = []
-  userNumber = parseInt(userInput.trim().split(" "))
-  for (let i=0; i<=userNumber; i++) {
-    inputArray.push(i.toString())
+  const newNumbers = userInput.trim()
+  const numberArray = newNumbers.split(" ")
+  numberArray.forEach(function(element){
+  if (element.includes("3")){
+    return "Won't you be my neighbor?"
+  }else if (element.includes("2")){
+    return "Boop!"
+  }else if (element.includes("1")){
+    return "Beep!"
+  }else {
+    return false
   }
-  return inputArray
+  })
+return numberArray  
 }
 
+
+
+
+  
+  
+  
+  
+  

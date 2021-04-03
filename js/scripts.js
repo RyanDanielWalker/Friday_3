d46f7fa69f77bd04270db37dc2cc7e5534ceddda
Ryan Walker <ryandanielwalker@gmail.com>
2021-04-02 17:57:06 -0700

    Finalize refactoring and styling.

--- js/scripts.js
+++ js/scripts.js
@@ -19,29 +19,18 @@ function mrRobogersSays(userInput) {
   }
   $(document).ready(function(){
     $("form#userForm").submit(function(event){
-    event.preventDefault()
-    const inputFromUser = parseInt($("#numberInput").val())
-    if (inputFromUser >= 0){
-      if (inputFromUser <= 1000) {
-        let printedResult = mrRobogersSays(inputFromUser)
-        $("#answerField").text("Mr. Robogers counts to that number like this: " + printedResult)
-        $("#answerField").slideDown(750)
+      event.preventDefault()
+      const inputFromUser = parseInt($("#numberInput").val())
+      if (inputFromUser >= 0){
+        if (inputFromUser <= 1000) {
+          let printedResult = mrRobogersSays(inputFromUser)
+          $("#answerField").text("Mr. Robogers counts to that number like this: " + printedResult)
+          $("#answerField").slideDown(750)
+        } else {
+          alert ("There are three ways to ultimate success: The first way is to be kind. The second way is to be kind. The third way is to follow the directions.")
+        }
       } else {
-        alert ("There are three ways to ultimate success: The first way is to be kind. The second way is to be kind. The third way is to follow the directions.")
+        alert("Let's try entering a number that is equally as worthwhile as you are.")
       }
-    } else {
-      alert("Let's try entering a number that is equally as worthwhile as you are.")
-    }
+    })
   })
-})
-
-
-
-
-
-
-
-
-
-
-  
